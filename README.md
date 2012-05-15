6172ee62f34e1fd90dea0c255034ee521ea67106
unknown <kuba@laptop.(none)>
2012-05-15 23:14:32 +0200

    Documentation improvements.
    * doc strings on functions improved
    * REAMDE updated

--- README.md
+++ README.md
@@ -3,3 +3,22 @@ userhome
 
 Simple os-independent tool for getting user home directory path.
 Currently works for posix systems and windows.
+
+
+usage
+-----
+
+Assuming, you're login on the system as "jacob", usage looks like this::
+
+
+   # On windows
+   >>> import userhome
+   >>> userhome.path() == 'C:\\Users\\jacob'
+   >>> userhome.path(".templates") == 'C:\\Users\\jacob\\.templates'
+   >>> userhome.path(".templates", "django") == 'C:\\Users\\jacob\\.templates\\django'
+
+   # On linux and other posix systems
+   >>> import userhome
+   >>> userhome.path() == '/home/jacob/'
+   >>> userhome.path(".templates") == '/home/jacob/.templates'
+   >>> userhome.path(".templates", "django") == '/home/jacob/.templates/django'
--- userhome.py
+++ userhome.py
@@ -1,13 +1,39 @@
 # -*- coding: utf-8 -*-
 import os
+from os import path as ospath
 
 
-def _posix_home():
-    return os.environ["HOME"]
+def _posix_home(*path):
+    """
+    Create paths starting with current user home directory.
+    This version works properly only for POSIX systems.
+
+    Assuming, you're login on the system as "jacob",
+    usage looks like this::
+
+       >>> import userhome
+       >>> userhome._posix_home() == '/home/jacob/'
+       >>> userhome._posix_home(".templates") == '/home/jacob/.templates'
+       >>> userhome._posix_home(".templates", "django") == '/home/jacob/.templates/django'
+    """
+    return ospath(os.environ["HOME"], *path)
 
 
-def _nt_home():
-    return os.path.join(os.environ["HOMEDRIVE"], os.environ["HOMEPATH"])
+def _nt_home(*path):
+    """
+    Create paths starting with current user home directory.
+    This version works properly only for NT systems.
+
+    Assuming, you're login on the system as "jacob",
+    usage looks like this::
+
+       >>> import userhome
+       >>> userhome._nt_home() == 'C:\\Users\\jacob'
+       >>> userhome._nt_home(".templates") == 'C:\\Users\\jacob\\.templates'
+       >>> userhome._nt_home(".templates", "django") == 'C:\\Users\\jacob\\.templates\\django'
+
+    """
+    return ospath.join(os.environ["HOMEDRIVE"], os.environ["HOMEPATH"], *path)
 
 
 _systems = {
@@ -16,6 +42,6 @@ _systems = {
 }
 
 
-def path():
+def path(*path):
     get_home_path = _systems[os.name]
-    return get_home_path()
+    return get_home_path(*path)
